00a8c58c8c8a9572eece9895f4dae5b9c5871f99
Sandra <radeskasandra1@gmail.com>
2025-09-30 12:50:33 +0200

    Fix PopUp rendering error

--- src/components/buttons/exchangeContactButton.js
+++ src/components/buttons/exchangeContactButton.js
@@ -1,5 +1,6 @@
 'use client';
 import { useState } from "react";
+import { createPortal } from "react-dom";
 import { contactMessages } from "@/lib/i18n";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faClose, faRepeat } from "@fortawesome/free-solid-svg-icons";
@@ -11,19 +12,22 @@ export default function ExchangeContactButton({page, user, lang = 'mk', ...props
   const messages = contactMessages[lang];
 
   return (
-    <div {...props}>
-      <button onClick={() => setShowExchangeContactForm(true)}>
-        <FontAwesomeIcon icon={faRepeat} className="w-6 h-6 pr-2" />
-        <span>{messages.buttonText}</span>
+    <>
+      <div {...props}>
+        <button onClick={() => setShowExchangeContactForm(true)}>
+          <FontAwesomeIcon icon={faRepeat} className="w-6 h-6 pr-2" />
+          <span>{messages.buttonText}</span>
         </button>
-      {showExchangeContactForm && (
+      </div>
+      {showExchangeContactForm && createPortal(
         <PopUp>
           <button onClick={() => setShowExchangeContactForm(false)} className="absolute top-2 right-3 text-lg font-bold" >
             <FontAwesomeIcon icon={faClose} className="w-5 h-5 text-2xl hover:text-[#3b82f6]" />
           </button>
           <ExchangeContactForm page={page} user={user} lang={lang} />
-        </PopUp>
+        </PopUp>,
+        document.body
       )}
-    </div>
+    </>
   );
 }
--- src/components/layout/popup.js
+++ src/components/layout/popup.js
@@ -1,7 +1,7 @@
 export default function PopUp({ children }) {
   return (
     <div className="fixed inset-0 bg-black/80 backdrop-blur flex items-center justify-center z-50">
-      <div className="bg-white p-10 max-w-xl w-full relative m-4">
+      <div className="bg-white p-10 max-w-xl w-full relative m-4 max-h-[90vh] overflow-y-auto">
         {children}
       </div>
     </div>
